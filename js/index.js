3275ed775dc655e4dcb95033ed59091ce65eb456
Luci Temple <lucitemple@yahoo.com.au>
2020-12-10 19:14:23 +1100

    Troublehoot - validation message now working

--- js/index.js
+++ js/index.js
@@ -1,35 +1,35 @@
 // Select the New Task Form
-const newTaskForm = document.querySelector("#newTaskForm");
+const newTaskForm = document.querySelector('#newTaskForm');
 
 // Add event listener for form submission
-newTaskForm.addEventListener("submit", (event)  => {
+newTaskForm.addEventListener('submit', (event)  => {
   // Prevent page from refreshing
   event.preventDefault();
 
   // Select New Task Form inputs, and store values.
-  const newTaskNameInput = document.querySelector("#newTaskNameInput");
+  const newTaskNameInput = document.querySelector('#newTaskNameInput');
   const taskName = newTaskNameInput.value;
 
-  const newTaskAssignedTo = document.querySelector("#newTaskAssignedTo");
+  const newTaskAssignedTo = document.querySelector('#newTaskAssignedTo');
   const assignedTo = newTaskAssignedTo.value;
 
-  const newTaskDescription = document.querySelector("#newTaskDescription");
+  const newTaskDescription = document.querySelector('#newTaskDescription');
   const taskDescription = newTaskDescription.value;
 
-  const newTaskStatus = document.querySelector("#newTaskStatus");
+  const newTaskStatus = document.querySelector('#newTaskStatus');
   const taskStatus = newTaskStatus.value;
 
-  const newTaskDueDate = document.querySelector("#newTaskDueDate");
+  const newTaskDueDate = document.querySelector('#newTaskDueDate');
   const dueDate = newTaskDueDate.value;
 
-  const formErrorMessage = document.querySelector("#formErrorMessage");
+  const formErrorMessage = document.querySelector('#formErrorMessage');
 
 // If invalid data, error message.
   if (!validFormFieldInput(taskName)) {
       formErrorMessage.innerHTML = "Invalid name. Please correct.";
-      formErrorMessage.getElementsByClassName.display = "block";
+      formErrorMessage.style.display = "block";
   } else {
-      formErrorMessage.getElementsByClassName.display = "none";
+      formErrorMessage.style.display = "none";
   }
 
 });
