d2f0803442fa7750ad6080f56bcb1b13958466aa
Luci Temple <lucitemple@yahoo.com.au>
2021-01-14 16:14:25 +1100

    Add test for addTask

--- test/test.js
+++ test/test.js
@@ -1,27 +1,18 @@
 const TaskManager = require("./../js/taskManager.js");
 const assert = require("assert");
 
-/* describe('TaskManager test suite', () => {
+describe('TaskManager test suite', () => {
   it('should add a task', () => {
     const taskManager = new TaskManager(0);
-    let len = taskManager.tasks.length;
-    taskManager.addTask('taskName', 'taskDescription', 'assignedTo', 'dueDate');
-    if (len < taskManager.tasks.length) {
-      assert.ok(taskManager.tasks[0].assignedTo, 'assignedTo');
+    let len = taskManager._tasks.length;
+    taskManager.addTask('Shopping', 'Buy groceries', 'Mary', '24/12/21');
+    if (len < taskManager._tasks.length) {
+      assert.ok(taskManager._tasks[0].assignedTo, 'assignedTo');
     }
   });
-}) */
 
-describe("TaskManager test suite", () => {
-  it("should add a task", () => {
+  it('should delete a task', () => {
     const taskManager = new TaskManager(0);
-    const expectedLength = 1;
-
-    const resultLength = taskManager.tasks.length;
-    taskManager.addTask('taskName', 'taskDescription', 'assignedTo', 'dueDate');
-
-    assert.ok(resultLength, expectedLength);
-
   });
-});
+})
 
