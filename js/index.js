480b6cfda90740b5fae55b49f7ef7b477f152c61
Luci Temple <lucitemple@yahoo.com.au>
2021-01-15 14:18:56 +1100

    Delete unnecessary commented code

--- js/index.js
+++ js/index.js
@@ -68,11 +68,6 @@ newTaskForm.addEventListener("submit", (event) => {
     // reset form
     document.querySelector("#newTaskForm").reset();
   }
-  // Clear form
-/*    newTaskNameInput.value = "";
-   newTaskDescription.value = "";
-   newTaskAssignedTo.value = "";
-   newTaskDueDate.value = ""; */
 });
 
 // Validate data to ensure not empty string or null
@@ -83,22 +78,12 @@ function validFormFieldInput(data) {
 // Calendar: future dates only, run on click
 newTaskDueDate.addEventListener("click", function () {
   let today = new Date();
-/*   let dateToday = today.getDate();
-  let monthToday = today.getMonth() + 1; */
-  var dateToday = String(today.getDate()).padStart(2, "0");
-  var monthToday = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+  let dateToday = String(today.getDate()).padStart(2, "0");
+  let monthToday = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
   let yearToday = today.getFullYear();
   let minDate = `${yearToday}-${monthToday}-${dateToday}`;
-  //alert("minDate " + minDate);
   newTaskDueDate.min = minDate;
 });
-
-/* function getFocus(inputID) {
-  document.getElementById(inputID).focus();
-}; */
-
-//const cardTemplate = document.querySelector("#cardTemplate");
-//const  = document.querySelector("#cardTemplate");
 
  // When done button clicked mark task done
 cardContainer.addEventListener("click", (event) => {
